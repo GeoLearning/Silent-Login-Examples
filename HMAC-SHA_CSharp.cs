e3ea92b472d0ccb7b94efa60c26f088c643424c0
Ryan Bergman <ryber@geolearning.com>
2011-03-14 14:06:42 -0500

    update C# example to show sha-256

--- HMAC-SHA_CSharp.cs
+++ HMAC-SHA_CSharp.cs
@@ -12,19 +12,33 @@ using System.Text;
 using System.Web;
 
 namespace MaestroSilentLogin {
-    class MaestroSilentLogin {
+    public class MaestroSilentLogin {
 
-        static void Main() {
+        public static void Main() {
+		/*
+		##########################################################
+		## The Secret Key
+		##  The Secret Key
+		##   Keys should never be written into code like this. Always practice security and good key managemnt.
+		##   see the NIST recomendations:
+		##   http://csrc.nist.gov/publications/nistpubs/800-57/SP800-57-Part1.pdf
+		##
+		##   note that this is not a real key you should get your key from Geolearning.
+		##   note also that CF uses pounds to signify output. So we need two punds here to escape the pound and produce only one.
+		###########################################################
+		*/
+		
             var msl = new MaestroSilentLogin();
-            msl.Launch("MyUserName", 42, "ijZeXCslP1Ou2s8T6bR3sa3Dk3KKtTDvKKEz0UT0x2K0P6qoWlmApK7EgzIe1us", "/geonext/MyCompany/trainingandmetricshome.geo?nav=TrainingandMetrics");
+            msl.Launch("geoadmin", 42, "4BDA179E-0BA6-9592-BB07B2CE6688AE3B", "/geonext/MyCompany/trainingandmetricshome.geo?nav=TrainingandMetrics");
         }
 
-        private void Launch(string username, int keyNumber, string secretKey, string originalURL)
+        public void Launch(string username, int keyNumber, string secretKey, string originalURL)
         {
             var timeStamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
 
+            // If useing sha1 then use the sha1login.geo endpoint
             var launchlink = new StringBuilder();
-            launchlink.Append("https://gm1.geolearning.com/geonext/MyCompany/sha1Login.geo");
+            launchlink.Append("https://gm1.geolearning.com/geonext/MyCompany/sha256Login.geo");
             launchlink.Append("?username=" + username);
             launchlink.Append("&timestamp=" + HttpUtility.UrlEncode(timeStamp));
             launchlink.Append("&id=" + keyNumber);
@@ -32,7 +46,6 @@ namespace MaestroSilentLogin {
             if(!String.IsNullOrEmpty(originalURL)){
                 launchlink.Append("&OriginalURL=" + HttpUtility.UrlEncode(originalURL));
             }
-
 
 
             //launch IE with the link
@@ -48,8 +61,9 @@ namespace MaestroSilentLogin {
             // Create a byte array from source data
             var tmpSource = Encoding.ASCII.GetBytes(aSourceData);
 
-            // Compute hash based on source data
-            var tmpHash = new SHA1CryptoServiceProvider().ComputeHash(tmpSource);
+            // Compute hash based on source data with either sha-1 or sha-256
+            var tmpHash = new SHA256Managed().ComputeHash(tmpSource);
+            //var tmpHash = new SHA1CryptoServiceProvider().ComputeHash(tmpSource);
 
             // convert the hash to hex
             var hmac = BitConverter.ToString(tmpHash).Replace("-", "");
